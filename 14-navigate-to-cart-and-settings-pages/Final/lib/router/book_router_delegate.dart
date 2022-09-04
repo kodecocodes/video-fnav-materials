de2861bc1e5a1bb64067478a00fec0d52b3f09a0
Shree Bhagwat <shreebhagwat94@gmail.com>
2022-09-05 00:33:56 +0530

    solved issues with ep 14

--- 14-navigate-to-cart-and-settings-pages/Final/lib/router/book_router_delegate.dart
+++ 14-navigate-to-cart-and-settings-pages/Final/lib/router/book_router_delegate.dart
@@ -1,9 +1,14 @@
 import '../helpers/book_data.dart';
+import '../screens/cart_screen.dart';
+import '../screens/checkout_screen.dart';
+import '../screens/home_screen.dart';
 import '../screens/login_screen.dart';
+import '../screens/my_books_screen.dart';
+import '../screens/settings_screen.dart';
+import '../screens/signup_screen.dart';
 import '../screens/splash_screen.dart';
 import 'app_state_manager.dart';
 import 'package:flutter/material.dart';
-import 'app_link.dart';
 
 class BookRouterDelegate extends RouterDelegate<AppStateManager>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin {
@@ -18,8 +23,7 @@ class BookRouterDelegate extends RouterDelegate<AppStateManager>
     appStateManager.addListener(notifyListeners);
     bookManager.addListener(notifyListeners);
   }
-  @override
-  Applink get currentConfiguration => getCurrentPath();
+
 
   @override
   Widget build(BuildContext context) {

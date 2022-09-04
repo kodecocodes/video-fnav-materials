d85c14b31859cfcf3dbdfbf60c7d155b751b9d5b
Shree Bhagwat <shreebhagwat94@gmail.com>
2022-09-04 22:03:03 +0530

    added materials for ep 14,15,16

---
+++ 15-navigate-to-detail-and-readbook-pages/Starter/lib/screens/detail_screen.dart
@@ -0,0 +1,152 @@
+// Copyright (c) 2022 Razeware LLC
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+// distribute, sublicense, create a derivative work, and/or sell copies of the
+// Software in any work that is designed, intended or marketed for pedagogical
+// or instructional purposes related to programming, coding, application
+// development, or information technology.  Permission for such use, copying,
+// modification, merger, publication, distribution, sublicensing, creation of
+// derivative works or sale is expressly withheld.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import 'package:books/helpers/book_data.dart';
+import 'package:flutter/material.dart';
+import '../helpers/constants.dart';
+import '../helpers/helper_function.dart';
+import 'read_book_screen.dart';
+import '../widgets/primary_button.dart';
+import '../widgets/subtitle_text_widget.dart';
+import '../widgets/title_text_widget.dart';
+
+import '../widgets/custom_app_bar.dart';
+
+class DetailsScreen extends StatefulWidget {
+  static MaterialPage page({Book? book, int index = -1}) {
+    return MaterialPage(
+        name: BookPages.detailsPath,
+        key: ValueKey(BookPages.detailsPath),
+        child: DetailsScreen(
+          bookData: book!,
+          index: index,
+        ));
+  }
+
+  const DetailsScreen({Key? key, required this.bookData, this.index = -1})
+      : super(key: key);
+
+  final Book bookData;
+  final int index;
+
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  final HelperFunction _helperFunction = HelperFunction();
+
+  @override
+  Widget build(BuildContext context) {
+    final width = _helperFunction.getWidth(context);
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: CustomAppBar(
+        title: 'Detail Screen',
+        showIcon: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    widget.bookData.image.toString(),
+                    width: 100,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: width - 140,
+                      child: TitleTextWidget(
+                        title: widget.bookData.name.toString(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                        width: width - 140,
+                        child: SubTitleTextWidget(
+                            title: widget.bookData.author.toString())),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                        width: width - 140,
+                        child: SubTitleTextWidget(
+                            title: widget.bookData.description.toString())),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SubTitleTextWidget(
+                        title:
+                            'Version: ' + widget.bookData.version.toString()),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                PrimaryButton(
+                    width: width * 0.5,
+                    title: 'Sample',
+                    onPressed: () {
+                      // Add navigator Nav 2.0 Function to navigate
+                      _helperFunction.navigateToPage(
+                          context, ReadBookScreen(bookData: widget.bookData));
+                    }),
+                PrimaryButton(
+                    width: width * 0.5,
+                    title: 'Buy Now',
+                    onPressed: () {
+                      cartItems.add(widget.bookData);
+                      _helperFunction.showToast(
+                          Colors.green, 'Book Added To Cart');
+                      print('Added to cart $cartItems');
+                    }),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ 16-go-to-previous-screen/Starter/lib/router/book_router_delegate.dart
@@ -0,0 +1,92 @@
+import '../helpers/book_data.dart';
+import '../screens/login_screen.dart';
+import '../screens/splash_screen.dart';
+import 'app_state_manager.dart';
+import 'package:flutter/material.dart';
+import 'app_link.dart';
+
+class BookRouterDelegate extends RouterDelegate<AppStateManager>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  final AppStateManager appStateManager;
+  final BookManager bookManager;
+
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  BookRouterDelegate({required this.bookManager, required this.appStateManager})
+      : navigatorKey = GlobalKey<NavigatorState>() {
+    appStateManager.addListener(notifyListeners);
+    bookManager.addListener(notifyListeners);
+  }
+  @override
+  Applink get currentConfiguration => getCurrentPath();
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      pages: [
+        if (!appStateManager.isInitialized) SplashScreen.page(),
+        if (appStateManager.isInitialized && !appStateManager.isLoggedIn)
+          LoginScreen.page(),
+
+        //Sign Up Screen
+        if (appStateManager.isInitialized && appStateManager.isSignUp)
+          SignupScreen.page(),
+
+        // Home Screen
+        if (appStateManager.isLoggedIn ||
+            !appStateManager.isSignUp && appStateManager.onSignUpComplete)
+          HomeScreen.page(),
+
+        //Cart Screen
+        if (appStateManager.onCart ||
+            appStateManager.onCart && bookManager.selectedIndex != -1 ||
+            appStateManager.onCart &&
+                appStateManager.onReadBook &&
+                bookManager.selectedIndex != -1)
+          CartScreen.page(),
+
+        // Settings Screen
+        if (appStateManager.onSettings ||
+            appStateManager.onSettings && bookManager.selectedIndex != -1 ||
+            appStateManager.onSettings &&
+                appStateManager.onReadBook &&
+                bookManager.selectedIndex != -1 &&
+                !appStateManager.onCart)
+          SettingsScreen.page(),
+
+        // Checkout Screen
+        if (appStateManager.onCheckout) CheckoutScreen.page(),
+
+        // My Book Screen
+        if (appStateManager.onMyBooks) MyBooksScreen.page(),
+
+        //Details Screen
+        if (bookManager.selectedIndex != -1 &&
+            !appStateManager.onCart &&
+            !appStateManager.onSettings)
+          DetailsScreen.page(
+              book: bookManager.selectedBookItem,
+              index: bookManager.selectedIndex),
+
+        // Read Book Screen
+        if (appStateManager.onReadBook &&
+            bookManager.selectedIndex != -1 &&
+            !appStateManager.onCart &&
+            !appStateManager.onSettings)
+          ReadBookScreen.page(bookManager.selectedBookItem)
+      ],
+    );
+  }
+
+  @override
+  Future<void> setNewRoutePath(configuration) async => null;
+
+  @override
+  void dispose() {
+    appStateManager.removeListener(notifyListeners);
+    bookManager.removeListener(notifyListeners);
+    super.dispose();
+  }
+}

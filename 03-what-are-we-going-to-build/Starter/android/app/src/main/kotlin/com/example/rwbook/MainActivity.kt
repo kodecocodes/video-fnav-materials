cee8cd69aeadf31a923cf457bc757bdda380d1e9
Shree Bhagwat <shreebhagwat94@gmail.com>
2022-09-04 18:50:00 +0530

    Added materials for ep 3 & 4

---
+++ 03-what-are-we-going-to-build/Starter/android/app/src/main/kotlin/com/example/rwbook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rwbook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 04-create-an-app-state-manager/Starter/lib/main.dart
@@ -0,0 +1,67 @@
+// Copyright (c) 2022 Razeware LLC
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+// distribute, sublicense, create a derivative work, and/or sell copies of the
+// Software in any work that is designed, intended or marketed for pedagogical
+// or instructional purposes related to programming, coding, application
+// development, or information technology.  Permission for such use, copying,
+// modification, merger, publication, distribution, sublicensing, creation of
+// derivative works or sale is expressly withheld.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
+
+void main() {
+  // Add Path Url Strategy to remove # from Url
+  runApp(const Book());
+}
+
+class Book extends StatefulWidget {
+  const Book({Key? key}) : super(key: key);
+
+  @override
+  State<Book> createState() => _BookState();
+}
+
+class _BookState extends State<Book> {
+
+  // TODO: Initialise book manager
+  // TODO: Initialise app state manager
+  // TODO: Initialise Router Parser
+  // TODO: Initialise Router Delegate
+
+
+  @override
+  void initState() {
+    super.initState();
+  // TODO: Declare Router Delegate
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Add MultiProvider
+    // TODO: Switch to MaterialApp router
+    return const  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
